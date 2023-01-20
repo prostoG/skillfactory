7e5e86fab3f9abffeafd855ae01aa604ce7443e0
prostoG <a.opk12@yandex.ru>
2023-01-20 16:06:33 +0300

    burger

--- src/script/index.js
+++ src/script/index.js
@@ -14,24 +14,35 @@ const swiper = new Swiper('.swiper', {
 // tads features
 
 const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
-	const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
 
-	for (const tab of tabsHandlerElems) {
-		tab.addEventListener('click', () => {
-			tabsHandlerElems.forEach(item => {
-				if (tab === item) {
-					item.classList.add('active');
-				} else {
-					item.classList.remove('active');
-				}
-			});
-
-			tabsFieldElems.forEach(item => {
-				if (item.dataset.tabsField === tab.dataset.tabsHandler) {
-					item.classList.remove('hidden')
-				} else {
-					item.classList.add('hidden')
-				}
-			});
+for (const tab of tabsHandlerElems) {
+	tab.addEventListener('click', () => {
+		tabsHandlerElems.forEach(item => {
+			if (tab === item) {
+				item.classList.add('active');
+			} else {
+				item.classList.remove('active');
+			}
 		});
-	};
+
+		tabsFieldElems.forEach(item => {
+			if (item.dataset.tabsField === tab.dataset.tabsHandler) {
+				item.classList.remove('hidden')
+			} else {
+				item.classList.add('hidden')
+			}
+		});
+	});
+};
+
+// burger 
+
+const menuBtn = document.querySelector('.header__burger-btn');
+const menuList = document.querySelector('.header__burger-menu-list');
+const headerLogo = document.querySelector('.header__logo');
+menuBtn.addEventListener('click', () => {
+	menuList.classList.toggle('active-burger');
+	menuBtn.classList.toggle('active-burger');
+	headerLogo.classList.toggle('active-burger')
+})
